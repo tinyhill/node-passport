7650d577b0a936daed793d8dcf4b51387b01f626
tinyhill <tinyhill@163.com>
2015-09-17 02:11:11 +0800

    用户登录同步注册

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -4,61 +4,132 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/profile/qq', function (req, res) {
-    request.get({
-        form: {
-            access_token: req.query.access_token
-        },
-        url: 'https://graph.z.qq.com/moc2/me'
-    }, function (err, response, body) {
-        if (err) {
-            res.json(err);
-        } else {
-            User.findOne({
-                qq_openid: body.replace(/^(.*)\=/, '')
+
+    var access_token = req.query.access_token;
+
+    request({
+        url: 'https://graph.qq.com/oauth2.0/me?access_token=' + access_token
+    }, function (error, response, body) {
+
+        if (error)
+            return res.json(error);
+
+        var result = JSON.parse(body.substring(body.indexOf('{'), body.lastIndexOf('}') + 1));
+
+        if (result.error)
+            return res.json(result);
+
+        request({
+            url: 'https://graph.qq.com/user/get_user_info?format=json' +
+            '&access_token=' + access_token +
+            '&oauth_consumer_key=' + result.client_id +
+            '&openid=' + result.openid
+        }, function (error, response, body) {
+
+            if (error)
+                return res.json(error);
+
+            var profile = JSON.parse(body);
+
+            if (profile.ret !== 0)
+                return res.json(profile);
+
+            User.findOrCreate({
+                qq_openid: result.openid
+            }, {
+                display_name: profile.nickname,
+                avatar: profile.figureurl_1,
+                gender: profile.gender,
+                province: profile.province,
+                city: profile.city
             }, function (err, user) {
                 res.json(err || user);
             });
-        }
+        });
     });
 });
 
 router.get('/profile/wechat', function (req, res) {
-    request.get({
-        form: {
-            access_token: req.query.access_token,
-            openid: req.query.openid,
-            lang: 'zh_CN'
-        },
-        url: 'https://api.weixin.qq.com/sns/userinfo'
-    }, function (err, response, body) {
-        if (err) {
-            res.json(err);
-        } else {
-            User.findOne({
-                qq_openid: JSON.parse(body).openid
-            }, function (err, user) {
-                res.json(err || user);
-            });
-        }
+
+    var access_token = req.query.access_token;
+    var openid = req.query.openid;
+
+    request({
+        url: 'https://api.weixin.qq.com/sns/userinfo?lang=zh_CN' +
+        '&access_token=' + access_token +
+        '&openid=' + openid
+    }, function (error, response, body) {
+
+        if (error)
+            return res.json(error);
+
+        var profile = JSON.parse(body);
+
+        if (profile.errcode)
+            return res.json(profile);
+
+        User.findOrCreate({
+            wechat_openid: openid
+        }, {
+            display_name: profile.nickname,
+            avatar: profile.headimgurl,
+            gender: profile.sex === '2' ? '女' : '男',
+            province: profile.province,
+            city: profile.city,
+            wechat_unionid: profile.unionid
+        }, function (err, user) {
+            res.json(err || user);
+        });
     });
 });
 
 router.get('/profile/weibo', function (req, res) {
+
+    var access_token = req.query.access_token;
+
     request.post({
         form: {
-            access_token: req.query.access_token
+            access_token: access_token
         },
         url: 'https://api.weibo.com/oauth2/get_token_info'
-    }, function (err, response, body) {
-        if (err) {
-            res.json(err);
-        } else {
-            User.findOne({
-                weibo_id: JSON.parse(body).uid
+    }, function (error, response, body) {
+
+        if (error)
+            return res.json(error);
+
+        var result = JSON.parse(body);
+
+        if (result.error)
+            return res.json(result);
+
+        request({
+            url: 'https://api.weibo.com/2/users/show.json?access_token=' + access_token +
+            '&source=' + result.appkey +
+            '&uid=' + result.uid
+        }, function (error, response, body) {
+
+            if (error)
+                return res.json(error);
+
+            var profile = JSON.parse(body);
+
+            if (profile.error)
+                return res.json(profile);
+
+            var loc = profile.location.split(' ');
+
+            User.findOrCreate({
+                weibo_id: profile.id
+            }, {
+                display_name: profile.screen_name,
+                avatar: profile.profile_image_url,
+                gender: profile.gender === 'f' ? '女' : '男',
+                province: loc[0],
+                city: loc[1]
             }, function (err, user) {
                 res.json(err || user);
             });
-        }
+        });
     });
 });
 
